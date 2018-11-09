e4c8ce1c6d955f417ca25f90d24e29a630451104
copernicus-mogley <ryan@z.cash>
2018-11-09 11:04:45 +0100

    troubleshooting

--- includes/lib/transifex-live-integration-hreflang.php
+++ includes/lib/transifex-live-integration-hreflang.php
@@ -123,8 +123,8 @@ class Transifex_Live_Integration_Hreflang {
 		// The problem starts here.
 		$site_url_slash_maybe = site_url();
 		$site_url = rtrim( $site_url_slash_maybe, '/' ) . '/';
-		die(print_r($site_url));
 		$unslashed_source_url = $site_url . $source_url_path;
+		die(print_r($unslashed_source_url));
 		$source_url = rtrim( $unslashed_source_url, '/' ) . '/';
 		$hreflang_out = '';
 		$hreflang_out .= <<<SOURCE
